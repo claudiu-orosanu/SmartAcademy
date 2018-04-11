f18ccf82b06dad40f63d9089942dba88056bc3c5
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2018-04-11 19:25:04 +0300

    Frontend - implement 'View courses' page - retrieve courses from backend API - add new elements to sidebar menu ('Courses' and 'Teachers')
    Backend
    - create 'courses' table in database
    - create 'Course' model
    - populate courses table with fake data
    - add package for Laravel autocompletion in PhpStorm

---
+++ Backend/app/Course.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Course
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $category
+ * @property string $image
+ * @property float $price
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Course extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'category',
+        'price',
+        'image',
+//        'teacher_id',
+    ];
+
+}
---
+++ Backend/app/Http/Resources/CourseResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->description,
+            'category' => $this->category,
+        ];
+
+        /*return [
+            'data' =>
+            [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->description,
+                'category' => $this->category,
+            ],
+            'links' => [
+                'self' => 'link-value',
+            ],
+        ];*/
+
+    }
+}
